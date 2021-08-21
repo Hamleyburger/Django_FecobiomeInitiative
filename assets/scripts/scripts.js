c91260788651268ff083cae1e919f7c6e52c5e9e
Hamley <MacDuck@hamleys-macbook-pro.home>
2021-08-21 17:15:04 +0200

    deployment settings

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,14 +3,29 @@
     const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
     // to find distance to title
     var title_position = $(".hero-title").position();
-    console.log(title_position);
+    //console.log(title_position);
  $('.nav-panel').toggleClass('scrolled', $(this).scrollTop() > title_position.top-60);
  });
 
 
 
 $(document).ready(function() {
+    // When element navbar-collapse has class "collapsing", toggle class "clicked" in nav-panel
     $(".navbar-toggler").click(function () {
-        $(".nav-panel").toggleClass("clicked");
+        // Super hackish function to detect whether navbar is growing or shrinking to try to 
+        // determine whether a click should trigger a solid background color or transparency
+        let firstmeasure;
+        let secondmeasure;
+        firstmeasure = $(".nav-panel").height()
+        setTimeout(function(){
+            secondmeasure = $(".nav-panel").height()
+            if (firstmeasure < secondmeasure) {
+                $(".nav-panel").addClass("expanded");
+            }
+            else {
+                $(".nav-panel").removeClass("expanded");
+            }
+        }, 60); // milliseconds. Can still fail if the navbar is both shrinking and growing within the time interval
     });
+
 });
