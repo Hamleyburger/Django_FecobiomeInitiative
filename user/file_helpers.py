bb08736dcd86d80f30b2ee54209d8913cc30baf9
Hamleyburger <alma9000@gmail.com>
2021-10-14 23:21:21 +0200

    backend/backup image processing and nicer modal preview

--- user/file_helpers.py
+++ user/file_helpers.py
@@ -1,4 +1,5 @@
 import magic
+import PIL
 
 
 def get_mimetype(file):
@@ -9,4 +10,44 @@ def get_mimetype(file):
     mime_type = magic.from_buffer(file.read(1024), mime=True)
     file.seek(initial_pos)
     return mime_type
+
+
+
+def resize_crop_image(file):
+    """ Resizes a square image and crops an unsquared image
+    (if for some reason javascript has been disabled in the
+    browser and user managed to upload anyway) \n\n
+    Returns PIL Image """
+
+    im = PIL.PIL_Image.open(file)
+    width, height = im.size   # Get dimensions
+    original_format = im.format
+    desired_width = 300
+    desired_height = 300
+
+    # Resize a square
+    if width == height:
+        if width > desired_width:
+            im = im.resize((desired_height, desired_width))
+
+    # Crop a rectangle:
+    else:
+        if width > desired_width or height > desired_height:
+
+            if width < desired_width:
+                desired_width = width
+            if height < desired_height:
+                desired_height = height
+
+            left = (width - desired_width)/2
+            top = (height - desired_height)/2
+            right = (width + desired_width)/2
+            bottom = (height + desired_height)/2
+
+            # Crop the center of the image
+            im = im.crop((left, top, right, bottom))
+
+    im.format = original_format
+
+    return im
 
